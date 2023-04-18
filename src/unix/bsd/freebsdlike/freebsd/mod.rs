2ebfb78195876a278d76ee927e98f71043dca9e1
Lewis Revill <lewis.revill@embecosm.com>
2023-04-18 16:30:48 +0100

    CHERI: Add morello+c64 to cfg checks

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -4595,7 +4595,7 @@ cfg_if! {
     } else if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use self::x86_64::*;
-    } else if #[cfg(target_arch = "aarch64")] {
+    } else if #[cfg(any(target_arch = "morello+c64", target_arch = "aarch64"))] {
         mod aarch64;
         pub use self::aarch64::*;
     } else if #[cfg(target_arch = "arm")] {
