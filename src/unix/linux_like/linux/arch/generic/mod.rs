b24091f516290988ea75d3c675ff20c5e090af6f
Lewis Revill <lewis.revill@embecosm.com>
2023-08-28 16:33:20 +0100

    CHERI: Quick patch to expose more defs

--- src/unix/linux_like/linux/arch/generic/mod.rs
+++ src/unix/linux_like/linux/arch/generic/mod.rs
@@ -95,6 +95,7 @@ cfg_if! {
     if #[cfg(all(any(target_arch = "x86",
                      target_arch = "x86_64",
                      target_arch = "aarch64",
+                     target_arch = "morello+c64",
                      target_arch = "loongarch64"),
                  not(any(target_env = "musl", target_env = "ohos"))))] {
         pub const SO_TIMESTAMP_NEW: ::c_int = 63;
@@ -113,6 +114,7 @@ cfg_if! {
                  target_arch = "x86_64",
                  target_arch = "arm",
                  target_arch = "aarch64",
+                 target_arch = "morello+c64",
                  target_arch = "riscv64",
                  target_arch = "s390x",
                  target_arch = "loongarch64"))] {
