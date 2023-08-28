3cff98261b316fc831385904520a95d8af790af1
Lewis Revill <lewis.revill@embecosm.com>
2023-08-28 16:33:20 +0100

    CHERI: Quick patch to expose more defs

--- src/unix/linux_like/linux/arch/generic/mod.rs
+++ src/unix/linux_like/linux/arch/generic/mod.rs
@@ -94,8 +94,10 @@ cfg_if! {
     // But they may still not have those _OLD ones.
     if #[cfg(all(any(target_arch = "x86",
                      target_arch = "x86_64",
-                     target_arch = "aarch64"),
-                 not(target_env = "musl")))] {
+                     target_arch = "aarch64",
+                     target_arch = "morello+c64",
+                     target_arch = "loongarch64"),
+                 not(any(target_env = "musl", target_env = "ohos"))))] {
         pub const SO_TIMESTAMP_NEW: ::c_int = 63;
         pub const SO_TIMESTAMPNS_NEW: ::c_int = 64;
         pub const SO_TIMESTAMPING_NEW: ::c_int = 65;
@@ -106,7 +108,14 @@ cfg_if! {
 }
 
 cfg_if! {
-    if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
+    if #[cfg(any(target_arch = "x86",
+                 target_arch = "x86_64",
+                 target_arch = "arm",
+                 target_arch = "aarch64",
+                 target_arch = "morello+c64",
+                 target_arch = "riscv64",
+                 target_arch = "s390x",
+                 target_arch = "loongarch64"))] {
         pub const FICLONE: ::c_ulong = 0x40049409;
         pub const FICLONERANGE: ::c_ulong = 0x4020940D;
     }
